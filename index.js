f448fbda17638befe35405d4f6a30f194bf4bb6b
xpeper <xpepermint@gmail.com>
2015-08-07 21:46:33 +0200

    adding Router, Route and RouteParser

---
+++ index.js
@@ -0,0 +1,9 @@
+var Router = require('./lib/Router');
+var Route = require('./lib/Route');
+var RouteParser = require('./lib/RouteParser');
+
+module.exports = {
+  Router: Router,
+  Route: Route,
+  RouteParser: RouteParser
+};

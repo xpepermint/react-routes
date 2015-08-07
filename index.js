15f6ecb31f6a160a8a69bf12d3c9cbea11840339
xpeper <xpepermint@gmail.com>
2015-08-07 23:39:11 +0200

    removing Route, Router routes attribute instead

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var Router = require('./lib/Router');
-var Route = require('./lib/Route');
 var RouteParser = require('./lib/RouteParser');
 
 module.exports = {
   Router: Router,
-  Route: Route,
   RouteParser: RouteParser
 };
